78b54cb51fd91ee88c64f7b41a7691c4490835a8
Karol Mocniak <karol@mocniak.pl>
2024-11-11 14:41:52 +0100

    booking route

---
+++ src/Booking/UI/BookingController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Booking\UI;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/booking')]
+#[AsController]
+final class BookingController
+{
+    public function __invoke(): Response
+    {
+        return new Response('<html><body><h1>Booking</h1></body></html>');
+    }
+}
