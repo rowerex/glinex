f17bacc5bb76be93ef33d5b6aba7e63ab7acd49c
Karol Mocniak <karol@mocniak.pl>
2024-11-11 14:01:09 +0100

    add resercation to Slot

--- src/Booking/Slot.php
+++ src/Booking/Slot.php
@@ -4,11 +4,19 @@ namespace App\Booking;
 
 class Slot
 {
+    private array $reservations;
     public function __construct(\DateTimeImmutable $param, \DateTimeImmutable $param1, int $int)
     {
+        $this->reservations = [];
     }
 
-    public function book(string $string)
+    public function book(string $string): void
     {
+        $this->reservations[] = $string;
+    }
+
+    public function countReservations(): int
+    {
+        return count($this->reservations);
     }
 }
